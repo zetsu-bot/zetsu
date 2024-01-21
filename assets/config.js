694a45b3b699a380901789defff3a736cd74140a
LeeAmsi666 <129909771+LeeAmsi666@users.noreply.github.com>
2024-01-21 15:10:26 +0700

    m

---
+++ assets/config.js
@@ -0,0 +1,47 @@
+let config = {
+  "Title": "Your Name - Links",
+  "Name": "Your Name",
+  "Description": "Your Description",
+  "links": [
+    {
+      "Title": "Website",
+      "URL": "https://t.me/ztsu_bot",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Github",
+      "URL": "https://github.com/zetsu-bot",
+      "icon_classes": "fab fa-github",
+    },
+    {
+      "Title": "Play Store",
+      "URL": "https://t.me/zetsu_channel",
+      "icon_classes": "fab fa-google-play"
+    },
+    {
+      "Title": "PC Games",
+      "URL": "https://t.me/zetsu_channel",
+      "icon_classes": "fab fa-itch-io"
+    },
+    {
+      "Title": "Blog",
+      "URL": "blog-test",
+      "icon_classes": "fas fa-blog"
+    },
+    {
+      "Title": "Twitter",
+      "URL": "https://twitter.com/ztsubot",
+      "icon_classes": "fab fa-twitter"
+    },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://twitter.com/ztsubot",
+      "icon_classes": "fab fa-linkedin"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://t.me/zetsu_channel",
+      "icon_classes": "fab fa-youtube"
+    },
+  ]
+}
